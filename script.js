efa92c6c8cd547348b6fb59dd51cdeb5fc7abcbe
Javieracof <javiitacm12@gmail.com>
2025-07-18 12:11:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const ramos = {
+  // PRIMER AÑO
+  "Cosmovisión Cristiana": ["Antropología Bíblica"],
+  "Taller de Habilidades de Aprendizaje I": ["Taller de Habilidades de Aprendizaje II"],
+  "Anatomía Humana": ["Fisiología Humana", "Anatomía en Salud Sexual y Reproductiva"],
+  "Biología Celular": ["Histoembriología para Obstetricia", "Bioquímica", "Microbiología e Inmunología"],
+  "Química General y Orgánica": ["Bioquímica"],
+  "Psicología del Desarrollo para Matronería": ["Psicología de la Salud"],
+  "Matronería y Fundamentos Socioculturales de la Salud": ["Anatomía en Salud Sexual y Reproductiva", "Fundamentos de Salud Sexual y Reproductiva"],
+  "Gestión Básica en Emergencias y Desastres": ["Atención en Matronería Basada en la Evidencia y Tecnología"],
+
+  "Antropología Bíblica": ["Ciencia y Religión"],
+  "Taller de Habilidades de Aprendizaje II": ["Trabajo Final de Carrera"],
+  "Fisiología Humana": ["Fisiopatología Humana"],
+  "Histoembriología para Obstetricia": [],
+  "Bioquímica": ["Farmacología General", "Microbiología e Inmunología"],
+  "Psicología de la Salud": ["Sexualidad, Salud y Género"],
+  "Anatomía en Salud Sexual y Reproductiva": [],
+  "Fundamentos de Salud Sexual y Reproductiva": ["Semiología y Patología Femenina Aplicada", "Sexualidad, Salud y Género"],
+  "Atención en Matronería Basada en la Evidencia y Tecnología": ["Procedimientos Básicos para la Atención Integral en Matronería"],
+
+  // SEGUNDO AÑO
+  "Ciencia y Religión": ["Enseñanzas de la Biblia"],
+  "Complementario I": ["Complementario II"],
+  "Inglés I": ["Inglés II"],
+  "Fisiopatología Humana": ["Promoción de la Salud"],
+  "Farmacología General": ["Promoción de la Salud", "Preclínico para la Atención Integral en Matronería", "Nutrición y Conducta Alimentaria en Salud Sexual y Reproductiva"],
+  "Microbiología e Inmunología": ["Preclínico para la Atención Integral en Matronería", "Microbiología Aplicada a Salud Sexual y Reproductiva"],
+  "Semiología y Patología Femenina Aplicada": [],
+  "Sexualidad, Salud y Género": [],
+  "Procedimientos Básicos para la Atención Integral en Matronería": ["Estrategias de Manejo del Parto Natural", "Preclínico para la Atención Integral en Matronería"],
+
+  "Enseñanzas de la Biblia": ["Familia y Sociedad"],
+  "Complementario II"
 
